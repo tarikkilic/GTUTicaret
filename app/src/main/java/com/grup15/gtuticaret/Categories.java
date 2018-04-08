313a1064edb730a4b2e88948cc392e4f54584241
akincam <akincam08@gmail.com>
2018-04-09 00:04:38 +0300

    kategoriler
    kategoriler

---
+++ app/src/main/java/com/grup15/gtuticaret/Categories.java
@@ -0,0 +1,58 @@
+package com.grup15.gtuticaret;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by AKIN Ç on 8.04.2018.
+ */
+
+public class Categories extends AppCompatActivity {
+    private DrawerLayout mdrawerLayout;
+    //control bar or navigation bar
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.categories);
+
+
+
+        //provides to the drawerlayout in activity_main.xml
+        mdrawerLayout=(DrawerLayout) findViewById(R.id.drawerLayout);
+        //takes four parametr
+        //first is the this main activity
+        //second is drawerlayout
+        //third and four parametrs takes string. Strings are in string.xml
+        actionBarDrawerToggle= new ActionBarDrawerToggle(this,mdrawerLayout,R.string.open,R.string.close);
+
+        mdrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        //provides home button is enable
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.basketmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    /**
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/grup15/gtuticaret/MainActivity.java
+++ app/src/main/java/com/grup15/gtuticaret/MainActivity.java
@@ -9,9 +9,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //ürünler ekranı
+        /*
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this,ProductScreen.class);
-        startActivity(intent);
+        startActivity(intent);*/
+        //kategori ekranı
+
+        setContentView(R.layout.categories);
+        Intent intent1 = new Intent(this,Categories.class);
+        startActivity(intent1);
+        //kategori içerikleri
+        /*
+        setContentView(R.layout.category_contents);
+        Intent intent2 = new Intent(this,Categories.class);
+        startActivity(intent2);
+        */
+
 
     }
 }
