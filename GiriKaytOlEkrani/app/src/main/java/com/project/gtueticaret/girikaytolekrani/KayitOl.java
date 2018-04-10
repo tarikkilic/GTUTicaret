199cddae076dd5ac458aabccbb26f61591f324f2
Burhan ELGÜN <belgun@gtu.edu.tr>
2018-04-10 09:16:18 +0300

    Giriş ve Kayıt Ol Ekranı

---
+++ GiriKaytOlEkrani/app/src/main/java/com/project/gtueticaret/girikaytolekrani/KayitOl.java
@@ -0,0 +1,16 @@
+package com.project.gtueticaret.girikaytolekrani;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class KayitOl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_kayitol);
+
+    }
+}
