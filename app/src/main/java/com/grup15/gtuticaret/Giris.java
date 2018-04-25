33c44fca017932b1124016465e972289faccbf92
Burhan ELGÜN <belgun@gtu.edu.tr>
2018-04-26 00:06:15 +0300

    Giriş ve Kayıt Olma Ekranları Tamamlandı

--- app/src/main/java/com/grup15/gtuticaret/Giris.java
+++ app/src/main/java/com/grup15/gtuticaret/Giris.java
@@ -1,20 +1,20 @@
 package com.grup15.gtuticaret;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Map;
 
 public class Giris extends AppCompatActivity  {
-
-    Map<String,String> ePostaVeParola;
 
 
     @Override
@@ -30,11 +30,59 @@ public class Giris extends AppCompatActivity  {
             {
                 Intent kayit= new Intent(Giris.this, KayitOl.class);
                 startActivity(kayit);
+
+
             }
 
         });
 
+        findViewById(R.id.giris).setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        try {
+                            File file = new File(getDir("data", MODE_PRIVATE), "KayitOl.ePostaVeParola");
+
+                            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+                            KayitOl.ePostaVeParola= (Map<String, String>) ois.readObject();
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+
+                        if(KayitOl.ePostaVeParola.get(((EditText)findViewById(R.id.epostaGiris)).getText().toString())!=null
+                                && ((EditText)findViewById(R.id.passwordGiris)).getText().toString()!=null
+                                && KayitOl.ePostaVeParola.get(((EditText)findViewById(R.id.epostaGiris)).getText().toString()).
+                                equals(((EditText)findViewById(R.id.passwordGiris)).getText().toString())){
+
+
+                            /*******************************************************************************/
+                            Intent kayit= new Intent(Giris.this, KayitOl.class);// celal ana sayfası başlayacak
+                            startActivity(kayit);
+                            /**********************************************************************************/
+                        }
+                        else if(KayitOl.ePostaVeParola.get(((EditText)findViewById(R.id.epostaGiris)).getText().toString())==null){
+                            /*******************************************************************************/
+                            //kullanıcı adı hatalıysa girer(celal)
+                            /*******************************************************************************/
+
+                        }
+                        else {
+                            /*******************************************************************************/
+                            //şifre hatalı mesajı ekrana verilecek(celal)
+                            /*******************************************************************************/
+
+                        }
+
+                    }
+                });
     }
 
 
+
 }
+
