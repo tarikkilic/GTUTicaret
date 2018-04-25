4497fb1b93a682df002b03134982bc8615afbfd4
tarikkilic <33003079+tarikkilic@users.noreply.github.com>
2018-04-26 00:35:58 +0300

    product üzerinde degisiklik

--- app/src/main/java/com/grup15/gtuticaret/Product.java
+++ app/src/main/java/com/grup15/gtuticaret/Product.java
@@ -10,21 +10,38 @@ import java.util.Stack;
 public class Product {
     private String name;
     private String features;
+    private String type;
     private double price;
-    private int numOfSell;
     private int id;
     private LinkedList<String> keyWords;
     private Stack<String> comments;
+    private String imageCode;
 
-    public Product(String name,String features,double price,int numOfSell,int id,
-                   LinkedList<String> keyWords,Stack<String> comments ){
-        this.name = name;
-        this.features = features;
-        this.price = price;
-        this.numOfSell = numOfSell;
-        this.id = id;
-        this.keyWords = keyWords;
-        this.comments = comments;
+    public Product(){
+        name = null;
+        features = null;
+        type = null;
+        price = 0;
+        id = 0;
+        keyWords = null;
+        comments = null;
+        imageCode = null;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getImageCode() {
+        return imageCode;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setImageCode(String imageCode) {
+        this.imageCode = imageCode;
     }
 
     public String getName() {
@@ -43,9 +60,6 @@ public class Product {
         return id;
     }
 
-    public int getNumOfSell() {
-        return numOfSell;
-    }
 
     public LinkedList<String> getKeyWords() {
         return keyWords;
@@ -53,5 +67,56 @@ public class Product {
 
     public Stack<String> getComments() {
         return comments;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFeatures(String features) {
+        this.features = features;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setKeyWords(LinkedList<String> keyWords) {
+        this.keyWords = keyWords;
+    }
+
+    public void setComments(Stack<String> comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (Double.compare(product.price, price) != 0) return false;
+        if (id != product.id) return false;
+        if (!name.equals(product.name)) return false;
+        if (!features.equals(product.features)) return false;
+        if (!keyWords.equals(product.keyWords)) return false;
+        return comments.equals(product.comments);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", features='" + features + '\'' +
+                ", price=" + price +
+                ", id=" + id +
+                ", keyWords=" + keyWords +
+                ", comments=" + comments +
+                '}';
     }
 }
