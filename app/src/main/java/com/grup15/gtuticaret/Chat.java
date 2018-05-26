5e884ac2b77b48489fd4d101f068cb4ba20cc39f
ramazanguvenc <32714915+ramazanguvenc@users.noreply.github.com>
2018-05-26 03:23:58 +0300

    Chat
    Artik kendi kendine konustugun mesajlar unutulmuyor.

--- app/src/main/java/com/grup15/gtuticaret/Chat.java
+++ app/src/main/java/com/grup15/gtuticaret/Chat.java
@@ -6,9 +6,18 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Chat extends MenuBar{
@@ -17,10 +26,13 @@ public class Chat extends MenuBar{
     private Message sms=new Message();
     //firebase den gelen tüm mesajları tutacak
     private LinkedList<Message> messages;
+    private LinkedList<Object> temp;
+    private HashMap hp;
     //mesaj gönderme butonu
     private FloatingActionButton send;
     //bu konusma sırasında ekranda smsleri gmstermek için
     private ArrayList<String> allsms=new ArrayList();
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Mesajlar");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +42,25 @@ public class Chat extends MenuBar{
         //super.menuBar();
         send = findViewById(R.id.send);
         messages=new LinkedList<>();
+        temp = new LinkedList<>();
+        hp = new HashMap();
         int count=0;
         //burası ilk açıldığında tüm mesajları yükler
         //normalde buraya linked listte size a kadar devam et denmeli ama sonsuz döngüye giriyor.
-        while (count<allsms.size()) {
-            showMessage(messages.get(count), messages.get(count).message,allsms);
-            count++;
-        }
+        mDatabase.child(Giris.whoami).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    HashMap hp = (HashMap) ds.getValue();
+                    Message ms = new Message((String)hp.get("message"),(String)hp.get("user"),(String)hp.get("send_time"));
+                    showMessage(ms,ms.message,allsms,true);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,8 +87,32 @@ public class Chat extends MenuBar{
         ListView list = (ListView) findViewById(R.id.sms_list);
         m.setMessage(text);
         //yeni mesajlar linked list e eklenir
+        mDatabase.child(Giris.whoami).child(String.valueOf(messages.size())).setValue(m);
         this.messages.addFirst(m);
+        /*burada mesajı ekrana eklemek için ilk önce stringBuildera ekler düzenler sonra stringe
+        ekleyip onu arrayliste atar.
+         */
+        StringBuilder s = new StringBuilder();
+        s.append(m.getSend_time());
+        s.append("\t"+"\t"+"\t"+"\t"+"\t"+"\t");
+        s.append(m.getUser());
+        s.append("\n");
+        s.append(m.message);
+        String ss= String.valueOf(s);
+        sms.add(ss);
+        String allsms[]=new String[sms.size()];
+        for(int i=0;i<sms.size();i++){
+            allsms[i]=sms.get(i);
+        }
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,android.R.id.text1, allsms);
+        list.setAdapter(adapter);
 
+    }
+    private void showMessage (Message m, String text,ArrayList<String> sms,boolean t){
+        ListView list = (ListView) findViewById(R.id.sms_list);
+        m.setMessage(text);
+        //yeni mesajlar linked list e eklenir
+        this.messages.addFirst(m);
         /*burada mesajı ekrana eklemek için ilk önce stringBuildera ekler düzenler sonra stringe
         ekleyip onu arrayliste atar.
          */
@@ -93,6 +141,15 @@ public class Chat extends MenuBar{
         private String user;
         //message time day/month/year
         private String send_time;
+
+        public Message(){
+
+        }
+        public Message(String m,String u,String s){
+            message = m;
+            user = u;
+            send_time = s;
+        }
 
         public String getSms_recipient() {
             return sms_recipient;
